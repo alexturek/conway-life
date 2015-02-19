167cfa59a2c66d4de7e68705fa498ede490e9d03
Alex Turek <alex@whitepages.com>
2015-02-18 20:39:59 -0800

    Adding a bit of rendering logic (which is broken), css, underscore

--- public/js/conway.js
+++ public/js/conway.js
@@ -1,5 +1,16 @@
 (function($) {
   $(function() {
-    $('')
+    var drawBoard = function(width, height) {
+      $('all-fields').empty();
+      var cells = _.map(
+        _.range(height), function(row_i) {
+          return _.map(
+            _.range(width), function(col_i) {
+              return '<div class="cell" id="' + (row_i * width + height) + '"</div>'
+            }).join('');
+        }).join('');
+      $('all-fields').html(cells);
+    };
+    drawBoard();
   })
 })(jQuery);
